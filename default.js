8d58495783ce2a28a167e6cd56e39136eedccd1f
Victor Lin <50209221+VictorVonLin@users.noreply.github.com>
2021-11-18 18:29:07 -0800

    Add files via upload
    testing...

--- default.js
+++ default.js
@@ -1,20 +1,19 @@
 var url = 'https://script.google.com/macros/s/AKfycbz8fCIZtUyOFt_HeJrEXl8PfiWdc1vXwPlvYlSWg3aNNdYLOARWGyg6px6pAfh2Ww0R/exec'
+var current = 'summary';
 
-
-
-
-
+var loader = '<div class="progress"><div class="indeterminate"></div></div>'
 
 function action(action, e) {
     var xhr = new XMLHttpRequest();
-    console.log(action);
+    $("#nav-mobile").children().removeClass("active");
+    $("#"+action).addClass("active");
+    $("main").html(loader);
+    // console.log(action);
     xhr.open('GET',  url + '?callback=ctrlq&action=' + action);
     xhr.onreadystatechange = function(e) {
       if (xhr.readyState === XMLHttpRequest.DONE &&
           xhr.status === 200) {
         var result = JSON.parse(e.target.response);
-        
-        console.log(result)
         $("main").html(result);
       }
     }
