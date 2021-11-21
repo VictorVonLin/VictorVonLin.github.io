350478b821b4f7c5a1ee1a4d7445d950dbbee7e2
Victor Lin <50209221+VictorVonLin@users.noreply.github.com>
2021-11-20 23:07:47 -0800

    Add files via upload
    loading + clipboard

--- default.js
+++ default.js
@@ -1,7 +1,7 @@
 var url = 'https://script.google.com/macros/s/AKfycbz8fCIZtUyOFt_HeJrEXl8PfiWdc1vXwPlvYlSWg3aNNdYLOARWGyg6px6pAfh2Ww0R/exec'
 var current = 'summary';
 
-var loader = '<div class="progress"><div class="indeterminate"></div></div>'
+var loader = '<div class="progress"><div class="indeterminate"></div></div><div style="margin-bottom:30vh; margin-top:15vh;" class="row container teal-text text-lighten-4 valign-wrapper"><h1 class="white-text vertical" style="margin-top:-1vh ;position:absolute; left:32vw; font-size: 15vh;"><b>弥助</b></h1><img class="col s1 left-align" style="z-index:999; margin-right:-22vw ; height: 50vh; width: auto;" src="attacking.gif"></img><h2 class="loading col s7" style="font-size: 5vw;margin-top:15vh;"><b>Exploring</b></h2></div>'
 
 function action(action, e) {
     var xhr = new XMLHttpRequest();
@@ -33,3 +33,4 @@ $(document).ready(function(){
 });
 
 action('summary');
+// $("main").html(loader);
